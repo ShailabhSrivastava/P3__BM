163cd2e086d4ffea2ace37b882c24144119ef9b4
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-09-20 12:58:24 +0530

    created model and controller

---
+++ src/controller/userController.js
@@ -0,0 +1,39 @@
+const userModel = require('../models/userModel');
+
+const createUser = async (req,res) => {
+    let data = req.body
+    let saveData = await userModel.create(data);
+    res.status(201).send({status : true, data : saveData});
+};
+
+const userLogin = async (req,res) => {
+    try{
+     const email = req.body.email;
+     const password = req.body.password;
+ 
+     let checkData = await userModel.findOne({email : email , password : password});
+     console.log(checkData);
+     if(!checkData) return res.status(400).send({status : false, msg: "This email and password is not exist"});
+ 
+     let token = jwt.sign({
+         authorId: checkData._id.toString(),
+         group: "group-8",
+       },"project3");
+ 
+       res
+         .status(200)
+         .send({
+           status: true,
+           message: "user Login Successful",
+           token: { token },
+         });
+ 
+    }catch(err)
+    {
+     console.log(err)
+    }
+ 
+ }
+
+module.exports.createuser = createUser;
+module.exports.login = userLogin;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('../src/route/route');
+const mongoose  = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+mongoose.connect("mongodb+srv://ShailabhSrivastava:LtR74yQBXKkSdvyd@cluster0.cxb6bki.mongodb.net/Shailabh-db", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
